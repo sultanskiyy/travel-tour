695a11cdae5ca8f7557b202433364eb372257113
sultanskiyy <sultanov_asadbek@icloud.com>
2026-03-09 19:28:00 +0500

    travel

---
+++ types/DestinationType.tsx
@@ -0,0 +1,11 @@
+export interface DestinationType {
+  id: string;
+  name?: string;
+  name_uz?: string;
+  slug?: string;
+  description?: string;
+  image?: string;
+  cover_image?: string;
+  icon?: string;
+  is_active?: boolean;
+};
