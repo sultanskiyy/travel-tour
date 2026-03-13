60a239a946d44d223aaeeadc079021bc50e678a2
sultanskiyy <sultanov_asadbek@icloud.com>
2026-03-13 16:08:36 +0500

    travel

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+import { Container } from 'lucide-react'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <Container>
+        <h1 className='text-3xl font-bold text-center mt-10'>Blog Page</h1>
+    </Container>
+    </>
+  )
+}
+
+export default page
